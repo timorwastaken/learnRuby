47ede871b2b7bbd5f7c8e3299192dedf08d28370
timorwastaken <tirah4022@live.com>
2024-10-22 14:16:57 -0400

    Initial commit for files from Learn Ruby book

---
+++ ex11p2.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+
+print "What is your name? "
+name = gets.chomp()
+print "Hello who is the love of your life? "
+love = gets.chomp()
+print "What is tomorrow? "
+ann = gets
+
+puts "Your name is #{name}, and the love of your life is #{love}. You love her so much and want ot be with her for ever and tomorrow is your 10 month #{ann} :)"
